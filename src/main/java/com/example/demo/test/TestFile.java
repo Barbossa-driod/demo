78b60375d27d6cee0b5e976efcdc3f64f1e5c784
Никита <graverman.777@gmail.com>
2021-12-14 17:12:34 +0300

    Initial Commit

---
+++ src/main/java/com/example/demo/test/TestFile.java
@@ -0,0 +1,11 @@
+package com.example.demo.test;
+
+
+import com.iba.service.filestoragestarter.repository.impl.FilePhotoRepositoryImpl;
+
+public class TestFile {
+
+    public void Check(){
+        FilePhotoRepositoryImpl filePhotoRepository = new FilePhotoRepositoryImpl();
+    }
+}
